b5f02b4f6a608c5f364286197a149d002cf16ab1
twitch7443 <twitch7443@protonmail.com>
2021-05-11 19:39:03 +0100

    Remove TODO entry as Spanish keys aren't possible without setting whole keyboard to ES

--- keyboards/jj40/keymaps/twitch7443/keymap.c
+++ keyboards/jj40/keymaps/twitch7443/keymap.c
@@ -16,9 +16,6 @@
 #include QMK_KEYBOARD_H
 #include "keymap_spanish.h"
 
-// TODO:
-// - Spanish Keys
-
 enum layers {
   _QWERTY = 0,
   _LOWER,
@@ -28,7 +25,9 @@ enum layers {
 };
 
 enum custom_keycodes {
-    OTTERLOVE = SAFE_RANGE
+    OTTERLOVE = SAFE_RANGE,
+    OTTERPLOVE,
+    OTTERHUG,
 };
 
 #define LOWER  MO(_LOWER)
@@ -69,29 +68,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
   _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,S(KC_NUHS),S(KC_NUBS),_______, _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
-),
-
+  ),
 /* Raise
  * ,-----------------------------------------------------------------------------------.
- * |   `  |  {[  |  }]  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |PrtSc |
+ * |   `  |      |  {[  |  ]}  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |PrtSc |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Del  |  (   |  )   |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |  \   |
+ * | Del  |      |  (   |  )   |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |  \   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |  {   |  }   |  F9  |  F10 |  F11 |  F12 |ISO # |ISO / |      |      |Enter |
+ * |      |      |  {   |  }   |  F10 |  F11 |  F12 |ISO # |ISO / |      |      |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_planck_mit( \
-  KC_GRV,  KC_LBRC, KC_RBRC,   KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_PSCR, \
-  KC_DEL,  KC_LPRN, KC_RPRN,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
-  _______, KC_LCBR, KC_RCBR,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, _______, _______, _______, \
+  KC_GRV,  _______, KC_LBRC,   KC_RBRC,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_PSCR, \
+  KC_DEL,  _______, KC_LPRN,   KC_RPRN,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
+  _______, _______, KC_LCBR,   KC_RCBR,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, _______, _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
 ),
 
 /* Adjust (Lower + Raise)
  * ,-----------------------------------------------------------------------------------.
- * |Reset | Otter|      |      |      |      |      |      |      |      |      |  Del |
+ * |Reset |Otter |OtterP|OtterH|      |      |      |      |      |      |      |  Del |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
@@ -101,7 +99,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] =  LAYOUT_ortho_4x12( \
-  RESET  , OTTERLOVE, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL, \
+  RESET  , OTTERLOVE, OTTERPLOVE, OTTERHUG, _______, _______, _______, _______, _______, _______, _______, KC_DEL, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
   BL_TOGG, BL_STEP, BL_ON  , BL_OFF , BL_INC , BL_DEC , ES_NTIL, _______, _______, _______, ES_IEXL, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
@@ -118,14 +116,27 @@ layer_state_t layer_state_set_user(layer_state_t state) {
   return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
 };
 
-
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
     case OTTERLOVE:
        if (record->event.pressed) {
            SEND_STRING(":otterLove5:");
        } else { // If it is being released
-           tap_code(KC_ENT)
+           tap_code(KC_ENT);
+       }
+       break;
+    case OTTERPLOVE:
+       if (record->event.pressed) {
+           SEND_STRING(":otterLoveP1:");
+       } else { // If it is being released
+           tap_code(KC_ENT);
+       }
+       break;
+    case OTTERHUG:
+       if (record->event.pressed) {
+           SEND_STRING(":otterHug2:");
+       } else { // If it is being released
+           tap_code(KC_ENT);
        }
        break;
     } 
