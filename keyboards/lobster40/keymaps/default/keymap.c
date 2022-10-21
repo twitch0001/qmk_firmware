ea2a3548d85aff9890d9be14300bf76bc3bd33d6
twitch0001 <twitch7443@protonmail.com>
2022-10-22 00:08:04 +0100

    Add Lobster40

--- keyboards/lobster40/keymaps/default/keymap.c
+++ keyboards/lobster40/keymaps/default/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_LOWER] = LAYOUT_ortho_4x12(
         KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    QK_BOOT,
         KC_TAB,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_K,    KC_ENTER,
-        KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_I,    KC_J,    KC_K,    KC_L,
+        KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_NUBS,    KC_B,    KC_N,    KC_M,    KC_I,    KC_J,    KC_K,    KC_L,
         KC_A,    KC_LCTL,    KC_LALT,    KC_LGUI,    _______,    KC_SPC,    _______,    KC_LEFT,    KC_DOWN,    KC_UP,    KC_RGHT ,    KC_LCTL
     ),
     /*
